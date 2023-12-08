0f52c2e21353a140dad942739a7bf4f11859542e
Keten991 <dwikusuma991@gmail.com>
2023-12-08 22:19:20 +0800

    bye dwi1

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from os.path import join, dirname
+from pymongo import MongoClient
+import jwt
+from datetime import datetime, timedelta
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+ 
