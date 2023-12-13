aa0c362e7cb188b4a1044d77fc107526538e2905
mualifkur <mualifihwan017@gmail.com>
2023-12-13 20:45:09 +0700

    by mualif

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import hashlib
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
+client = MongoClient('mongodb+srv://test:sparta@cluster0.z9fd051.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
 app = Flask(__name__)
 
 
@@ -18,17 +21,23 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route('/api/sign_up' , methods=['POST'])
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/sign_up' , methods=['POST'])
 def sign_up():
-    username = request.get('username')
-    password = request.get('password')
+    username = request.form('username')
+    password = request.form('password')
+    count = db.user.count_documents({})
+    num = count + 1
     doc = {
+        'num' :num, 
         'username' : username,
         'password' : password,
     }
-    db.akun.insert_one(doc)
-    return jsnotify({'result': 'success', 
-                     'msg': f'Account, {username} created successfully'})
+    db.user.insert_one(doc)
+    return jsonify({'msg': f'Account, {username} created successfully'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
