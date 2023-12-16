69dc5dbfe8bf1e384cd78a56ddb0ff33001bbe4d
Keten991 <dwikusuma991@gmail.com>
2023-12-16 16:57:20 +0800

    bye dwi

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import hashlib
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-Mongo_client = 'mongodb+srv://test:sparta@cluster0.i8ofwto.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
+Mongo_client = ''
 client = MongoClient(Mongo_client)
 
 db = client.projectAkhir
@@ -22,8 +22,7 @@ def index():
 
 @app.route('/login')
 def login():
-    msg = request.args.get('msg')
-    return render_template('login.html', msg=msg)
+    return render_template('login.html')
 
 @app.route('/signup')
 def signup():
@@ -34,48 +33,66 @@ def diskusi():
     token_receive = request.cookies.get("mytoken")
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"username": payload["id"]})
+        user_info = db.user.find_one({"id": payload["id"]})
         return render_template('forum_diskusion.html', user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="Your token has expired"))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="There was problem logging you in"))
 
-@app.route("/sign_in", methods=["POST"])
-def sign_in():
-    # Sign in
-    username_receive = request.form["username_give"]
-    password_receive = request.form["password_give"]
-    pw_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
-    result = db.users.find_one(
-        {
-            "username": username_receive,
-            "password": pw_hash,
-        }
-    )
-    if result:
+# @app.route("/sign_in", methods=["POST"])
+# def sign_in():
+#     # Sign in
+#     username_receive = request.form["username_give"]
+#     password_receive = request.form["password_give"]
+#     pw_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
+#     result = db.user.find_one(
+#         {
+#             "id": username_receive,
+#             "pw": pw_hash,
+#         }
+#     )
+#     if result:
+#         payload = {
+#             "id": username_receive,
+#             # the token will be valid for 24 hours
+#             "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24),
+#         }
+#         token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+#         return jsonify(
+#             {
+#                 "result": "success",
+#                 "token": token,
+#             }
+#         )
+#     # Let's also handle the case where the id and
+#     # password combination cannot be found
+#     else:
+#         return jsonify(
+#             {
+#                 "result": "fail",
+#                 "msg": "We could not find a user with that id/password combination",
+#             }
+#         )
+@app.route("/api/login", methods=["POST"])
+def api_login():
+    id_receive = request.form["id_give"]
+    pw_receive = request.form["pw_give"]
+
+    pw_hash = hashlib.sha256(pw_receive.encode("utf-8")).hexdigest()
+    result = db.user.find_one({"id": id_receive, "pw": pw_hash})
+    if result is not None:
         payload = {
-            "id": username_receive,
-            # the token will be valid for 24 hours
+            "id": id_receive,
             "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24),
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
-
-        return jsonify(
-            {
-                "result": "success",
-                "token": token,
-            }
-        )
-    # Let's also handle the case where the id and
-    # password combination cannot be found
+        return jsonify({"result": "success", "token": token})
     else:
         return jsonify(
-            {
-                "result": "fail",
-                "msg": "We could not find a user with that id/password combination",
-            }
-        )
+            {"result": "fail", "msg": "Either your email or your password is incorrect"}
+        )   
 
 @app.route('/sign_up' , methods=['POST'])
 def sign_up():
