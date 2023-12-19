1818b9ecd4def3266ab3695c9a8b83412897dbae
Keten991 <dwikusuma991@gmail.com>
2023-12-19 08:52:17 +0800

    push

--- app.py
+++ app.py
@@ -127,6 +127,7 @@ def save_article():
     db.article.insert_one(doc)
     return jsonify ({'massage':'data was saved!!!'})
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
  
